650a3ec6c7563b5bb71971dbc8a30e73de54817e
brsaca <brsaca@gmail.com>
2023-10-10 12:56:15 -0600

    Initial Commit

---
+++ FintechUI/FintechUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FintechUIApp.swift
+//  FintechUI
+//
+//  Created by Brenda Saavedra Cantu on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FintechUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
