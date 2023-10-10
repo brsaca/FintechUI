61325b56b5cbb1de1b925f77c6dd915acb4710f8
brsaca <brsaca@gmail.com>
2023-10-10 13:26:37 -0600

    feat: initial folder structure, colors

---
+++ FintechUI/Extension/Color.swift
@@ -0,0 +1,15 @@
+//
+//  Color.swift
+//  FintechUI
+//
+//  Created by Brenda Saavedra Cantu on 10/10/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let cGray = Color("darkGray")
+    static let cGreen = Color("green")
+    static let cYellow = Color("yellow")
+    static let cPurple = Color("purple")
+}
