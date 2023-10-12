d93299ccecac0b465d77f91c9b50c813b546b197
brsaca <brsaca@gmail.com>
2023-10-11 18:24:48 -0600

    feat: add numbersPad functionality

---
+++ FintechUI/Core/Send/Components/DialPad.swift
@@ -0,0 +1,77 @@
+//
+//  DialPad.swift
+//  FintechUI
+//
+//  Created by Brenda Saavedra Cantu on 11/10/23.
+//
+
+import SwiftUI
+
+enum Pads: String, CaseIterable {
+    case n1 = "1"
+    case n2 = "2"
+    case n3 = "3"
+    case n4 = "4"
+    case n5 = "5"
+    case n6 = "6"
+    case n7 = "7"
+    case n8 = "8"
+    case n9 = "9"
+    case minus = "<"
+    case n0 = "0"
+    case dot = "."
+}
+
+struct DialPad: View {
+    // MARK: View Properties
+    let pad: Pads
+    @Binding var currentAmount: String
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Text(pad.rawValue)
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+            Spacer()
+        }
+        .frame(width: (UIScreen.main.bounds.width - 70) / 3, height: 70)
+        .background(Color.cGray.opacity(0.9))
+        .cornerRadius(20)
+        .onTapGesture {
+            print("init currentAmount:: \(currentAmount)")
+            if currentAmount.isEmpty {
+                if pad != .minus && pad != .dot && pad != .n0 {
+                    currentAmount.append(pad.rawValue)
+                }
+            } else {
+                if pad == .minus {
+                    currentAmount.removeLast()
+                } else if pad != .dot {
+                    currentAmount.append(pad.rawValue)
+                } else if !currentAmount.contains(".") {
+                    currentAmount.append(".")
+                }
+            }
+            
+            print("final currentAmount:: \(currentAmount)")
+        }
+    }
+}
+
+// MARK: - Previews
+#Preview("0"){
+    DialPad(pad: .n0, currentAmount: .constant(""))
+}
+
+#Preview("1"){
+    DialPad(pad: .n1, currentAmount: .constant("100"))
+}
+
+#Preview("minus empty"){
+    DialPad(pad: .minus, currentAmount: .constant(""))
+}
+
+#Preview("minus 100"){
+    DialPad(pad: .minus, currentAmount: .constant("100"))
+}
--- FintechUI/Core/Send/View/SendView.swift
+++ FintechUI/Core/Send/View/SendView.swift
@@ -11,6 +11,20 @@ struct SendView: View {
     // MARK: View Properties
     let contact: User
     @Environment(\.dismiss) var dismiss
+    @State var currentAmount:String = String()
+    
+    var formattedAmount: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "$"
+        formatter.maximumFractionDigits = 2
+        
+        if let number = Double(currentAmount) {
+            return formatter.string(from: NSNumber(value: number)) ?? "$0.00"
+        }
+        
+        return "$0.00"
+    }
     
     var body: some View {
         VStack(spacing: 26) {
@@ -21,9 +35,16 @@ struct SendView: View {
             ContactInfo
             
             // Amount
-            
+            TextField("$0.00", text: .constant(formattedAmount))
+                .font(.system(size: 60))
+                .foregroundColor(.white)
+                .font(.headline)
+                .multilineTextAlignment(.center)
+                .disabled(true)
+                .padding()
             
             // Pad
+            NumberPad
             
             Spacer()
             // Button
@@ -51,7 +72,7 @@ extension SendView {
             
             Spacer()
             
-            ButtonWithIcon(type: .square, image: "ellipsis", action: {})
+            ButtonWithIcon(type: .square, image: "bubble.left.and.text.bubble.right", action: {})
         }
     }
     
@@ -73,6 +94,14 @@ extension SendView {
                     .font(.callout)
                     .foregroundStyle(.gray)
                     .padding(.top, -10)
+            }
+        }
+    }
+    
+    var NumberPad: some View {
+        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 3), spacing: 10) {
+            ForEach(Pads.allCases, id: \.self) { pad in
+                DialPad(pad: pad, currentAmount: $currentAmount)
             }
         }
     }
